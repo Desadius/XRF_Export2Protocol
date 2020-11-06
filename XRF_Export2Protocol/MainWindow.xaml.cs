3e35eef8625a51aa7abedc87372940bf3001170b
Desadius <desadius@centrum.cz>
2020-11-06 14:44:06 +0100

    Den 2 - základ UI - WIP

--- XRF_Export2Protocol/MainWindow.xaml.cs
+++ XRF_Export2Protocol/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +22,25 @@ namespace XRF_Export2Protocol
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string PathToCVS = null;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                PathToCVS = openFileDialog.FileName;
+                
+            }
+        }
+
+        private void MenuItemClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
